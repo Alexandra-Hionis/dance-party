2d567346790d4d4c27dc7ef7c3847c43f6698baf
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2024-02-23 10:56:37 -0500

    revised ux by giving the option to end the dance party or further proceed with site function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,14 @@
 const buttonStartDanceParty = document.querySelector('.button-start-dance-party');
 const buttonGiphy = document.querySelector('.button-giphy');
 buttonStartDanceParty.onclick = function() {
-  buttonStartDanceParty.classList.add('dance-party');
+ // when button is clicked, add class name dance-party to be toggled
+ buttonStartDanceParty.classList.toggle('dance-party');
   if (buttonStartDanceParty.classList.contains('dance-party')) {
-    buttonStartDanceParty.style.display = "none";
+    buttonStartDanceParty.innerHTML = "party's over :("
     // show giphy button
     buttonGiphy.style.display = "block";
     console.log("start dance party! Let's dance button shown");
+  } else {
+    window.location.reload();
   }
 };

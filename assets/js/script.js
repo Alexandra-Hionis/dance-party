27668c6ea2240cdbffbc05c42a21b576f71f4ac1
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2024-02-23 10:30:01 -0500

    very basic button with onclick event. When button clicked, log string to console and hide button

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+const buttonStartDanceParty = document.querySelector('.button--start-dance-party');
+
+buttonStartDanceParty.onclick = function() {
+  buttonStartDanceParty.classList.add('dance-party');
+  if (buttonStartDanceParty.classList.contains('dance-party')) {
+    buttonStartDanceParty.style.display = "none";
+    console.log("start dance party! Shoot confetti");
+  }
+};

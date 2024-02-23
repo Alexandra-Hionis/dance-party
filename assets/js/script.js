18cd725c5e42641a09a6b336a1038b8882c1cc6d
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2024-02-23 10:37:49 -0500

    second button added. when click me is pressed, it hides and giphy trigger button is displayed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
 
 const buttonStartDanceParty = document.querySelector('.button-start-dance-party');
-
+const buttonGiphy = document.querySelector('.button-giphy');
 buttonStartDanceParty.onclick = function() {
   buttonStartDanceParty.classList.add('dance-party');
   if (buttonStartDanceParty.classList.contains('dance-party')) {
     buttonStartDanceParty.style.display = "none";
-    console.log("start dance party! Shoot confetti");
+    // show giphy button
+    buttonGiphy.style.display = "block";
+    console.log("start dance party! Let's dance button shown");
   }
 };
